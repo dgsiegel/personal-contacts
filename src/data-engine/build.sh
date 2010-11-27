c9266f653838caf5067c072c4916983d14d1cb39
Salomon Sickert <sickert@in.tum.de>
2010-11-27 17:17:40 +0100

    Added simple build.sh

---
+++ src/data-engine/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+valac main.vala abstract-factory.vala group.vala group-manager.vala individual.vala individual-manager.vala persona.vala attribute/attribute.vala attribute/email.vala attribute/jabber.vala attribute/phone.vala 
